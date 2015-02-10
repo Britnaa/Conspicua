035ba59c1647d6a31bada62ee5c4a43f40b4cbba
Britt G <bag62@drexel.edu>
2015-02-09 20:32:44 -0500

    Project (as of 2/6/15)

---
+++ Assets/Scripts/CameraDad.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDad : MonoBehaviour {
+	
+	public Transform[] pivots;
+	public int currPivot = 0;
+	public Texture2D overlayImage;
+	public Rect overlay;
+
+	
+	// Use this for initialization
+	void Start () {
+		NextPivot();
+		overlay = new Rect (0, 0, Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) NextPivot();
+	}
+	
+	void NextPivot(){
+		currPivot+=1;
+		if(currPivot>=pivots.Length) currPivot = 0;
+		transform.position = pivots[currPivot].position;
+	}
+
+	void onGui() {
+		if(Input.GetKeyDown(KeyCode.Space))
+		GUI.DrawTexture (overlay, overlayImage);
+		}
+}	
