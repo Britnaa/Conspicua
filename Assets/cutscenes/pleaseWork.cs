f0b89dc3a602b89d3dcb50c11fdde4a8779f408a
Britnaa <bag62@drexel.edu>
2015-05-29 11:57:58 -0400

    audio, cut scenes, infirmary pass

---
+++ Assets/cutscenes/pleaseWork.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pleaseWork : MonoBehaviour {
+	
+		public MovieTexture movTexture;
+		void Start() {
+			GetComponent<Renderer>().material.mainTexture = movTexture;
+			movTexture.Play();
+
+		}
+	
+}
